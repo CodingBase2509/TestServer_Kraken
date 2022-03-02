8dfe02281ddfae3bfc956e6ffcbb9ede6d445245
Fabian Dasler <fabian@MacBook-Pro-von-Fabian.fritz.box>
2022-03-02 21:38:09 +0100

    add commandline arguments, dont know wether it works or not

--- Kraken_Testserver/Launcher.cs
+++ Kraken_Testserver/Launcher.cs
@@ -72,7 +72,6 @@ public class Launcher
         s.AppendLine("start without flags will start both servers");
         s.AppendLine(string.Empty);
         s.AppendLine("-h    opens this help page");
-        s.AppendLine("Das ist ein test");
 
         Console.WriteLine(s);
 
